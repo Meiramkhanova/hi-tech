dea5efed7bdc2b41e28afc3b778344d8ea5b71c1
adinamei <adinaone04@gmail.com>
2025-09-15 12:16:57 +0500

    fix: cache

--- src/entities/header/headerService.service.ts
+++ src/entities/header/headerService.service.ts
@@ -5,7 +5,14 @@ import { HeaderData, HeaderResponse } from "./getHeader.t";
 export const getHeaderData = async (): Promise<HeaderData[]> => {
   try {
     const response: AxiosResponse<HeaderResponse> = await api.get(
-      "navlinks?populate=*"
+      "navlinks?populate=*",
+      {
+        headers: {
+          "Cache-Control": "no-cache",
+          Pragma: "no-cache",
+          Expires: "0",
+        },
+      }
     );
 
     // если Strapi вернул undefined или пустоту → подставим []
--- src/entities/homepage/HomePage.service.ts
+++ src/entities/homepage/HomePage.service.ts
@@ -5,7 +5,14 @@ import { HomePageData, HomePageResponse } from "./getHomepage.t";
 export const getHomePageData = async (): Promise<HomePageData | null> => {
   try {
     const response: AxiosResponse<HomePageResponse> = await api.get(
-      "homepage?populate[swiper][populate]=*&populate[about][populate]=*&populate[aboutinfos][populate]=*&populate[mainNews][populate]=*"
+      "homepage?populate[swiper][populate]=*&populate[about][populate]=*&populate[aboutinfos][populate]=*&populate[mainNews][populate]=*",
+      {
+        headers: {
+          "Cache-Control": "no-cache",
+          Pragma: "no-cache",
+          Expires: "0",
+        },
+      }
     );
 
     // если Strapi вернул undefined или пустоту → подставим []
