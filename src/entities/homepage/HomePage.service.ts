7357423f88391707f96ae5ff32432facebd09bd2
adinamei <adinaone04@gmail.com>
2025-09-15 13:37:55 +0500

    fix: cache

--- src/entities/homepage/HomePage.service.ts
+++ src/entities/homepage/HomePage.service.ts
@@ -1,24 +1,21 @@
-import { AxiosResponse } from "axios";
-import { api } from "../api";
-import { HomePageData, HomePageResponse } from "./getHomepage.t";
+import { HomePageData } from "./getHomepage.t";
 
 export const getHomePageData = async (): Promise<HomePageData | null> => {
   try {
-    const response: AxiosResponse<HomePageResponse> = await api.get(
-      "homepage?populate[swiper][populate]=*&populate[about][populate]=*&populate[aboutinfos][populate]=*&populate[mainNews][populate]=*&",
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/homepage?populate[swiper][populate]=*&populate[about][populate]=*&populate[aboutinfos][populate]=*&populate[mainNews][populate]=*`,
       {
-        headers: {
-          "Cache-Control": "no-cache",
-          Pragma: "no-cache",
-          Expires: "0",
-        },
+        cache: "no-store", // ключ! всегда свежие данные, Vercel не отдаст старую страницу
       }
     );
 
-    // если Strapi вернул undefined или пустоту → подставим []
-    return response.data?.data ?? null;
+    if (!res.ok) throw new Error("Ошибка при запросе к Strapi");
+
+    const json = await res.json();
+
+    return json.data ?? null;
   } catch (error) {
-    console.error("Ошибка при получении данных для header:", error);
-    return null; // сайт не упадёт, а просто будет пустое меню
+    console.error("Ошибка при получении данных для homepage:", error);
+    return null;
   }
 };
