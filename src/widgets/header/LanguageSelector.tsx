77b7d76189bb591814d0f7b34e0b5236a6b7c4f7
adinamei <adinaone04@gmail.com>
2025-10-14 17:52:09 +0500

    feat: language selector

--- src/widgets/header/LanguageSelector.tsx
+++ src/widgets/header/LanguageSelector.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Select,
   SelectContent,
@@ -6,6 +8,9 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { cn } from "@/lib/utils";
+import { usePathname, useRouter } from "next/navigation";
+
+const locales = ["kk", "ru", "en"];
 
 function LanguageSelector({
   className,
@@ -16,9 +21,23 @@ function LanguageSelector({
   selectClassName?: string;
   iconClassName?: string;
 }) {
+  const router = useRouter();
+
+  const pathname = usePathname();
+
+  const changeLanguage = (locale: string) => {
+    const currentLocale = pathname.split("/")[1];
+
+    const pathWithoutLocale = locales.includes(currentLocale)
+      ? pathname.replace(`/${currentLocale}`, "")
+      : pathname;
+
+    router.push(`/${locale}${pathWithoutLocale}`);
+  };
+
   return (
     <div className={cn("lg-switcher", className)}>
-      <Select>
+      <Select onValueChange={changeLanguage}>
         <SelectTrigger
           className={cn(
             "w-[4.5rem] !h-14 rounded-2xl items-center font-light",
@@ -28,9 +47,9 @@ function LanguageSelector({
           <SelectValue placeholder="Рус" />
         </SelectTrigger>
         <SelectContent align="end" className="font-light">
-          <SelectItem value="light">Кз</SelectItem>
-          <SelectItem value="dark">Рус</SelectItem>
-          <SelectItem value="system">Англ</SelectItem>
+          <SelectItem value="kk">Кз</SelectItem>
+          <SelectItem value="ru">Рус</SelectItem>
+          <SelectItem value="en">Англ</SelectItem>
         </SelectContent>
       </Select>
     </div>
