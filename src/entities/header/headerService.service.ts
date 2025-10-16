10c334137e0534c6e322561c7a0623d14a28df59
adinamei <adinaone04@gmail.com>
2025-10-16 11:36:13 +0500

    feat: locale for service

--- src/entities/header/headerService.service.ts
+++ src/entities/header/headerService.service.ts
@@ -1,9 +1,12 @@
+import { getLocale } from "next-intl/server";
 import { HeaderData } from "./getHeader.t";
 
 export const getHeaderData = async (): Promise<HeaderData[]> => {
+  const locale = await getLocale();
+
   try {
     const res = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/navlinks?populate=*`,
+      `${process.env.NEXT_PUBLIC_API_URL}/navlinks?populate=*&locale=${locale}`,
       {
         cache: "no-store",
       }
